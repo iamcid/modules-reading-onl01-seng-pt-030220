2f8eb5a060abbeb88c0471710f52c4a18fea8a8c
Christopher Cid <iamcid17@gmail.com>
2020-04-04 04:40:18 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,14 +3,14 @@ require_relative './class_methods_module.rb'
 
 class Kid
   include Dance
+  
+  extend MetaDancing
 
   attr_accessor :name
   
   def initialize(name)
     @name = name
   end
-  
-  extend MetaDancing
   
   
 end
